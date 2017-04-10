66310e82ad89bcf90ab35e266f51802eb572ed1f
Ben Lambeth <lambethben@gmail.com>
2017-04-09 22:43:43 -0500

    Removed CP2 test cases

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -86,95 +86,6 @@ int32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes) {
     return 0;
 }
 
-
-void rtc_test(){
-    clear();
-    set_cursor(0, 0);
-    uint32_t rtc_freq = BASE_RTC_FREQ;
-    uint32_t dummy;
-    uint32_t fd_rtc = sys_open((uint8_t*)"/dev/rtc");
-    kbd_t k;
-    sys_write(fd_rtc, &rtc_freq, 4);
-    enable_irq(8);
-    while(1){
-        sys_read(fd_rtc, &dummy, 4);
-        printf("1");
-        while (kbd_read(0, &k, 2)) {
-            if(kbd_equal(k, F4_KEY)){
-                rtc_freq <<= 1;
-                if(rtc_freq > RTC_MAX_FREQ)
-                    rtc_freq = BASE_RTC_FREQ;
-                sys_write(fd_rtc, &rtc_freq, 4);
-            }else if(kbd_equal(k, F5_KEY)){
-                disable_irq(8);
-                sys_close(fd_rtc);
-                return;
-            }
-        }
-    }
-}
-
-void list_files(){
-    uint32_t it;
-    clear();
-    set_cursor(0,0);
-    uint8_t dir[] = ".";
-    int32_t fd = sys_open(dir);
-    int8_t text[33];
-    fstat_t data;
-    while((sys_read(fd, text, 33) != 0)) {
-        text[32] = 0;
-        printf("File Name: %s",text);
-        for(it = 0; it < 35 - strlen(text); it++)
-            printf(" ");
-        sys_stat(fd, &data, sizeof(fstat_t));
-        printf("File Type: %d    File Size: %dB\n", data.type, data.size);
-    }
-    sys_close(fd);
-}
-
-void list_file_name(uint8_t * name){
-    clear();
-    set_cursor(0,0);
-    int32_t fd = sys_open(name);
-    fstat_t data;
-    sys_stat(fd, &data, sizeof(fstat_t));
-    uint32_t size = data.size + 1;
-    int8_t text[size];
-    sys_read(fd, text, size-1);
-    text[size - 1] = '\0';
-    sys_write(1, text, size);
-    printf("\nFile Name: %s", name);
-    sys_close(fd);
-}
-
-int32_t list_file_index(int32_t index){
-    uint32_t it = 0;
-    clear();
-    set_cursor(0,0);
-    uint8_t dir[] = ".";
-    int32_t fd = sys_open(dir);
-    uint8_t name[33];
-    int8_t test[33];
-    while((sys_read(fd, name, 33) != 0) && it < index) {it++;}
-    if(sys_read(fd, test, 33) == 0)
-        index = 0;
-    else
-        index++;
-    name[32] = 0;
-    uint32_t fd2 = sys_open(name);
-    fstat_t data;
-    if(sys_stat(fd2, &data, sizeof(fstat_t)) == 0){
-        int8_t text[data.size];
-        sys_read(fd2, text, data.size);
-        sys_write(1, text, data.size);
-    }
-    printf("\nFile Name: %s\n", name);
-    sys_close(fd2);
-    sys_close(fd);
-    return index;
-}
-
 /* int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes)
  * Decription: Reads from the keyboard, displaying text and returning a line
  *             to the user
@@ -186,22 +97,12 @@ int32_t list_file_index(int32_t index){
  */
 int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes) {
     kbd_t k;
-    uint32_t i = 0;
-    uint32_t index = 0;
-    uint8_t name[] = "frame0.txt";
+    int32_t i = 0;
     int8_t a;
     nbytes = nbytes > 128 ? 128 : nbytes; //Cap line size at 128
     while (i < nbytes) { //Keep reading until nbytes
         if (kbd_read(0, &k, 2)){ //Read a single key
-            if(kbd_equal(k, F1_KEY)){ //List files for checkpoint 2
-                list_files();
-            } else if(kbd_equal(k, F2_KEY)){ //List file by name for checkpoint 2
-                list_file_name(name);
-            } else if(kbd_equal(k, F3_KEY)){ //List file by index for checkpoint 2
-                index = list_file_index(index);
-            } else if(kbd_equal(k, F4_KEY)){ //Test the rtc for checkpoint 2
-                rtc_test();
-            } else if(kbd_equal(k, L_KEY) && k.ctrl) { //Reset the screen
+            if(kbd_equal(k, L_KEY) && k.ctrl) { //Reset the screen
                 clear();
                 set_cursor(0, 0);
                 i = 0;
